f19b946967dd8161a03976077443949479f248fd
Your Name <you@example.com>
2025-02-05 16:11:34 +0300

    ПЗ 3 Права доступа

--- django_rest/rest/snippets/views.py
+++ django_rest/rest/snippets/views.py
@@ -4,7 +4,10 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 @csrf_exempt
@@ -45,7 +48,21 @@ def snippet_detail(request, pk):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
