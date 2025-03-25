9518a9ca671d76a492b5ea1c8f5a70b9918779b0
Your Name <you@example.com>
2025-03-25 09:18:50 +0300

    Лекция 9 Отношения сериализаторов

--- parser/parser/serializers.py
+++ parser/parser/serializers.py
@@ -16,9 +16,39 @@ class CommentSerializer(serializers.Serializer):
 
 
 Account = serializers.CharField()
+Album = serializers.CharField()
+AlbumSerializer = serializers.CharField()
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'account_name', 'users', 'created']
         read_only_fields = ['account_name']
+
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = serializers.SlugRelatedField(
+    many=True,
+    read_only=True,
+    slug_field='title'
+    )
+    class Meta:
+        model = Album
+        fields = ['album_name', 'artist', 'tracks']
+        qs = Album.objects.all()
+        print(AlbumSerializer(qs, many=True).data)
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, related_name='tracks', on_delete=models.CASCADE)
+    order = models.IntegerField()
+    title = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        unique_together = ['album', 'order']
+        ordering = ['order']
+        def __str__(self):
+            return '%d: %s' % (self.order, self.title)
+
