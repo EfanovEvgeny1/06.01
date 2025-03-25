6999581260affb68ab94fc7d7b04416e6597a6fd
Your Name <you@example.com>
2025-03-25 09:41:11 +0300

    Лекция 10 Резрешения

--- parser/parser/serializers.py
+++ parser/parser/serializers.py
@@ -53,3 +53,24 @@ class Track(models.Model):
         def __str__(self):
             return '%d: %s' % (self.order, self.title)
 
+class BlocklistPermission(permissions.BasePermission):
+    """
+    Global permission check for blocked IPs.
+    """
+    def has_permission(self, request, view):
+        ip_addr = request.META['REMOTE_ADDR']
+        blocked = Blocklist.objects.filter(ip_addr=ip_addr).exists()
+        return not blocked
+    
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+            # Instance must have an attribute named `owner`.
+            return obj.owner == request.user
